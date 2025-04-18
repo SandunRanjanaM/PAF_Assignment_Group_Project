3a1c431ffd8e86ae9a5edf027dc8e71de5f85747
SandunRanjanaM <sandunranjana2001@gmail.com>
2025-04-18 12:14:08 +0530

    backend endpoints for Learning Plans

---
+++ backend/src/main/java/com/sliit/backend/models/LearningPlan.java
@@ -0,0 +1,28 @@
+package com.sliit.backend.models;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+import java.util.List;
+
+@Document(collection = "LearningPlans")
+@Getter
+@Setter
+public class LearningPlan {
+    @Id
+    private String id;
+    private String userId;
+    private String progressName;
+    private Date createdAt;
+    private Date updatedAt;
+    private String title;
+    private String description;
+    private List<String> steps;
+    private int durationValue;        
+    private String durationUnit; 
+    private String priority;    
+    private boolean isCompleted;
+}
