5c9f73375c4f143b16d54e1333acd5be3d8612a0
SandunRanjanaM <sandunranjana2001@gmail.com>
2025-04-20 15:59:37 +0530

    backend changes for Learning Progress

---
+++ backend/src/main/java/com/sliit/backend/LearningProgressRepository.java
@@ -0,0 +1,14 @@
+package com.sliit.backend;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface LearningProgressRepository extends MongoRepository<LearningProgress, String> {
+    List<LearningProgress> findByUserId(String userId);
+    List<LearningProgress> findByProgressNameOrderByCreatedAtDesc(String progressName);
+    Optional<LearningProgress> findTopByUserIdAndProgressNameOrderByCreatedAtDesc(String userId, String progressName);
+}
