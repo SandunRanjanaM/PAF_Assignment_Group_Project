63e97e63b2a24e52868e6c5c27b8485fc32493fa
SandunRanjanaM <sandunranjana2001@gmail.com>
2025-04-24 18:28:24 +0530

    change backend

--- backend/src/main/java/com/sliit/backend/BackendApplication.java
+++ backend/src/main/java/com/sliit/backend/BackendApplication.java
@@ -1,17 +1,21 @@
 package com.sliit.backend;
-
+ 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+ 
 @SpringBootApplication
 @RestController
 public class BackendApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+ 
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+ 
+    @GetMapping("/")
+    public String rootEndpoint() {
+        String message = "Hello World!";
+        return message;
+    }
 }
