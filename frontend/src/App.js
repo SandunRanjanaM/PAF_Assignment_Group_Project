870d50ee672796536917bde0a56f5200761df476
Tharindu23891 <tdissanayaka2002@gmail.com>
2025-05-06 17:44:01 +0530

    update_app_js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import CreateLearningProgress from './components/CreateLearningProgress';
-import ViewAllLearningProgress from './components/ViewAllLearningProgress';
-import CreateLearningPlan from './components/CreateLearningPlan';
-import ViewAllPlan from './components/ViewAllPlan';
-import UpdateLearningPlan from './components/UpdateLearningPlan';
-import UpdateLearningProgress from './components/UpdateLearningProgress'; 
+// import CreateLearningProgress from './components/CreateLearningProgress';
+// import ViewAllLearningProgress from './components/ViewAllLearningProgress';
+// import CreateLearningPlan from './components/CreateLearningPlan';
+// import ViewAllPlan from './components/ViewAllPlan';
+// import UpdateLearningPlan from './components/UpdateLearningPlan';
+// import UpdateLearningProgress from './components/UpdateLearningProgress'; 
 
+import CreateComment from './components/CreateComment';
+import NotificationViewer from './components/NotificationViewer';
+import ViewCommentsByPost from './components/ViewCommentsByPost';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<CreateLearningProgress />} />
+        {/* <Route path="/" element={<CreateLearningProgress />} />
         <Route path="/progresses" element={<ViewAllLearningProgress />} />
         <Route path="/create-plan" element={<CreateLearningPlan />} />
         <Route path="/view-all-plans" element={<ViewAllPlan />} />
         <Route path="/update-plan/:id" element={<UpdateLearningPlan />} />
         <Route path="/update-progress/:progressId" element={<UpdateLearningProgress />} /> 
-        <Route path="/update-plan-by-user/:userId/:progressName" element={<UpdateLearningPlan />} />
+        <Route path="/update-plan-by-user/:userId/:progressName" element={<UpdateLearningPlan />} /> */}
+
+        <Route path="/CreateComment" element={<CreateComment />} />
+        <Route path="/NotificationViewer" element={<NotificationViewer />} />
+        <Route path="/ViewCommentsByPost/:postId" element={<ViewCommentsByPost />} />
+
+
       </Routes>
     </Router>
   );
